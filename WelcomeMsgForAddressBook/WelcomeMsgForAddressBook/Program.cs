ed22cc6700d2f65993201ff2416cf1baa5a4c3bf
thakareshubh <shubhamthakare032@gmail.com>
2022-03-10 17:08:05 +0530

    creat Welcome massage in address book project

---
+++ WelcomeMsgForAddressBook/WelcomeMsgForAddressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WelcomeMsgForAddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+        }
+    }
+}
