e5d422ba08d2fcf4890b163656c43dec5e8a8daa
thakareshubh <shubhamthakare032@gmail.com>
2022-03-10 17:10:20 +0530

    creat address book project

---
+++ Uc-1-AddressBook/Uc-1-AddressBook/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Uc_1_AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Person p1 = new Person();
+            p1.Address();
+            
+        }
+        class Person
+        {
+            
+               
+            public void Address()
+            {
+                Console.WriteLine("Enter your First name");
+                string FirstName=Console.ReadLine();
+                Console.WriteLine("Enter your last name");
+                string LastName=Console.ReadLine();
+                Console.WriteLine("Enter your State");
+                string State=Console.ReadLine();
+                Console.WriteLine("Enter your city");
+                string City=Console.ReadLine();
+                Console.WriteLine("Enter zip");
+                string Zip =Console.ReadLine();
+                Console.WriteLine("Enter phone number");
+                string PhoneNumber=Console.ReadLine();
+                Console.WriteLine("Enter your mail");
+                string Email=Console.ReadLine();
+
+            }
+
+        }
+    }
+}
